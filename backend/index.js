eec3a736d3c08cfdf2bff10ef754355111d27d7a
Satya <rsatya180@gmail.com>
2026-04-18 21:47:19 +0530

    phn

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,12 @@ const PORT = process.env.PORT || 5000;
 
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type"]
+}));
+
 app.use(express.json());
 
 // Test route
